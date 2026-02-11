8a9366a7558bbd246dc9527c3a0dea2016ef07d7
awb99 <awb99@gmail.com>
2026-02-11 17:36:40 -0500

    datahike version

--- src/bug/tuple.clj
+++ src/bug/tuple.clj
@@ -2,8 +2,7 @@
   (:require
    [nano-id.core :refer [nano-id]]
    [datahike.api :as d]
-   [bug.db :refer [start-asset-db add-update-list get-list ]]
-   ))
+   [bug.db :refer [start-asset-db add-update-list get-list]]))
 
 
 (def dbconn (start-asset-db "./db"))
@@ -11,23 +10,22 @@
 (repeatedly 5 #(nano-id 8))
 
 (defn make-list [name c]
-           {:lists/name name
-            :lists/asset (->> (repeatedly c #(nano-id 8))
-                               (into []))
-            })
+  {:lists/name name
+   :lists/asset (->> (repeatedly c #(nano-id 8))
+                     (into []))})
 
 (add-update-list dbconn (make-list "small" 5))
 
 (-> (get-list dbconn "small")
     :lists/asset
-    count) 
+    count)
 ;; 5
 
 (add-update-list dbconn (make-list "medium" 900))
 
 (-> (get-list dbconn "medium")
     :lists/asset
-    count) 
+    count)
 ;; 900 
 
 (add-update-list dbconn (make-list "big" 3150))
